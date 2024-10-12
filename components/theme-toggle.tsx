7dcabcb463c172417adcd3860d0534e3b531aaca
Ismael Melo <ismael@labofcodes.com>
2024-10-12 21:41:00 +0100

    Ajustes 3

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -26,13 +26,13 @@ export function ModeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          Branco
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          Preto
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          Padrão
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
